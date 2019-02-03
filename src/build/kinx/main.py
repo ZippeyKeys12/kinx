9341b0db5da6cef6f76e22657741274275029361
ZippeyKeys12 <ZippeyKeys12@gmail.com>
2019-02-03 00:12:19 -0600

    Horizontal line
    And unhandled tag printing

--- src/build/kinx/main.py
+++ src/build/kinx/main.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from json import load as load_json
 from os.path import join as path_join
 from subprocess import call
@@ -5,13 +6,12 @@ from typing import Dict, Tuple, Union
 
 from frontmatter import load as load_fm
 from jinja2 import Environment, FileSystemLoader
-from mistune import Markdown
 from lxml import etree
 from lxml.html import HTMLParser
+from mistune import Markdown
 
 from .lexer import BlockLexer, InlineLexer
 from .renderer import Renderer
-from collections import OrderedDict
 
 
 class Builder:
@@ -21,7 +21,7 @@ class Builder:
         self.path = path
         self.dev = dev
 
-        kinxfile = load_fm(self._get_path("Kinxfile"))
+        kinxfile = load_fm(self.__get_path("Kinxfile"))
 
         self.markdown = Markdown(
             renderer=Renderer(escape=False, hard_wrap=True),
@@ -35,7 +35,8 @@ class Builder:
             jinja_extensions = ()
         try:
             self.env = Environment(
-                extensions=jinja_extensions, loader=FileSystemLoader(kinxfile["root"])
+                extensions=jinja_extensions,
+                loader=FileSystemLoader(self.__get_path(kinxfile["root"])),
             )
         except:
             print("Unexpected error: 'root' key not found in Kinxfile")
@@ -48,22 +49,28 @@ class Builder:
         for i in etree.fromstring(self.markdown(kinxfile.content), HTMLParser())[0]:
             if i.tag == "ul":
                 if current_header in project_dir:
-                    project_dir[current_header].extend(self._get_links_md(i))
+                    project_dir[current_header].extend(self.__get_links_md(i))
                 else:
-                    project_dir[current_header] = self._get_links_md(i)
-            elif i.tag[:1] == "h" and int(i.tag[1:]) in range(1, 7):
+                    project_dir[current_header] = self.__get_links_md(i)
+            elif i.tag[:1] == "h" and i.tag[1:] in (str(j) for j in range(1, 7)):
                 current_header = (i.text, i.tag[1:])
+            else:
+                project_dir[current_header].append(i.tag)
+                if i.tag not in ["hr"]:
+                    print("{} is not read in Kinxfile".format(etree.tostring(i)))
         del current_header
 
         self.kinxfile = {}
         for i in ["title", "author", "description", "url", "copyright", "theme"]:
             self.kinxfile[i] = kinxfile[i]
-        self.kinxfile["content"] = project_dir
+        del kinxfile
+
+        self.kinxfile["content"] = OrderedDict(project_dir)
         del project_dir
 
         print(self.kinxfile)
 
-    def _get_links_md(self, element):
+    def __get_links_md(self, element):
         sets = []
 
         for i in element:
@@ -71,7 +78,7 @@ class Builder:
 
             for k in i:
                 if k.tag == "ul":
-                    n = self._get_links_md(k)
+                    n = self.__get_links_md(k)
 
                     if len(n) > 1:
                         tags.append(n)
@@ -83,7 +90,7 @@ class Builder:
 
         return sets
 
-    def _get_path(self, path_to: str) -> str:
+    def __get_path(self, path_to: str) -> str:
         return path_join(self.path, path_to)
 
     def build(self):
